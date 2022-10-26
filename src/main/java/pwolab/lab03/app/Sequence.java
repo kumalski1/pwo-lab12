83e1956bab634bb7803d095afa770f1863d93c03
student <student@localhost>
2022-10-26 19:52:13 +0200

    Dystrybucja 1

--- src/main/java/pwolab/lab03/app/Sequence.java
+++ src/main/java/pwolab/lab03/app/Sequence.java
@@ -8,6 +8,11 @@ package pwolab.lab03.app;
 
  public static void main(String[] args) {
 
- (new SeqToFileApp()).run(args);
+ switch(args.length){
+ case 3: (new SeqToOutApp()).run(args); break;
+ case 4: (new SeqToFileApp()).run(args); break;
+ default: System.out.println("!Illegal arguments\n"
+ + "Legal usage: seqName from to [fileName]");
+ }
  }
  }
